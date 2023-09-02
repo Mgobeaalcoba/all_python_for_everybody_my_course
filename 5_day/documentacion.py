8bd654d203cef326067ab3b69e158d6c2d1fafd4
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-09-02 15:31:13 -0300

    :memo: Add documentation tips

--- 5_day/documentacion.py
+++ 5_day/documentacion.py
@@ -16,3 +16,10 @@ marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
 conjuntos_aislados = marcas_smartphones.isdisjoint(marcas_tv)
 
 print(conjuntos_aislados)
+
+A = {1, 2, 3}
+B = {4, 5, 6}
+C = {6, 7, 8}
+
+print('A and B are disjoint:', A.isdisjoint(B))
+print('B and C are disjoint:', B.isdisjoint(C))
