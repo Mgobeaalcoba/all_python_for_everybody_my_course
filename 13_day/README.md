5900cb388fc7de06a60f2219c27d57cb7af5bfcb
Mariano Gobea Alcoba <gobeamariano@gmail.com>
2023-09-22 10:58:05 -0300

    :sparkles: Identified lenguage in the os

--- 13_day/README.md
+++ 13_day/README.md
@@ -1,1 +1,15 @@
 # My own voice assistant
+
+Si deseas descargar lenguajes adicionales en tu sistema operativo Windows puedes encontrar las instrucciones acá: 
+https://support.microsoft.com/es-es/topic/descargar-idiomas-y-voces-para-lector-inmersivo-el-modo-lectura-y-lectura-en-voz-alta-4c83a8d8-7486-42f7-8e46-2b0fdf753130
+
+Para Mac aquí:
+https://support.apple.com/es-es/guide/mac-help/mh26684/mac#:~:text=En%20el%20Mac%2C%20selecciona%20el,en%20%E2%80%9CIdioma%20y%20regi%C3%B3n%E2%80%9D%20.&text=Haz%20clic%20en%20General.,y%20haz%20clic%20en%20A%C3%B1adir.
+
+Para Linux así:
+
+```bash
+sudo apt-get install espeak-ng-espeak
+```
+
+En el ejemplo de arriba estaríamos instalando el español si es que no lo tenés! 
--- 13_day/main.py
+++ 13_day/main.py
@@ -8,16 +8,18 @@ import datetime
 import wikipedia
 
 def run():
-    desgrabar()
+    # desgrabar()
+    hablar("Hello World!!! This is my first message. I'm your own voice assistant programmed with Python. Congratulations!")
+    # distintas_voces()
 
-# Escucha nuestro microfono y devuelve el audio como texto - desgrabar:
+# Escucha nuestro micrófono y devuelve el audio como texto - desgrabar:
 def desgrabar():
     # almacenar el recognizer en variable
     r = sr.Recognizer()
 
-    # configuramos el microfono
+    # configuramos el micrófono
     with sr.Microphone() as origen:
-        # Tiempo de espera desde que se active el microfono
+        # Tiempo de espera desde que se active el micrófono
         r.pause_threshold = 0.8 # menos de 1 segundo
 
         # Informar que comenzó la grabación para saber que ya comenzó
@@ -28,7 +30,7 @@ def desgrabar():
 
         try:
             # Buscar en google lo que haya escuchado
-            pedido = r.recognize_google(audio, lenguaje= "es-ar")
+            pedido = r.recognize_google(audio, language="es-ar")
 
             # Prueba de que pudo ingresar y transformar nuestra vos:
             print("Dijiste: " + pedido)
@@ -58,6 +60,30 @@ def desgrabar():
 
             # Devolver error
             return "sigo esperando"
-        
+
+# Transformar la respuesta de nuestro asistente (por ahora en texto) a una voz humana
+def hablar(mensaje):
+    # Encender el motor de pyttsx3
+    engine = pyttsx3.init() # Inicio el motor
+
+    # Seteo el idioma que deseo usar para mi asistente de voz:
+    engine.setProperty('voice', distintas_voces())
+
+    # Pronunciar mensaje
+    engine.say(mensaje)
+    engine.runAndWait()
+
+def distintas_voces() -> str:
+    engine = pyttsx3.init()
+
+    # Devuelve las voces disponibles en mi sistema operativo:
+    for voice in engine.getProperty('voices'):
+        print(voice)
+
+    # Guardo los id's en variables para luego poder usarlos si fuese necesario:
+    id1 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
+
+    return id1
+
 if __name__ == '__main__':
     run()
